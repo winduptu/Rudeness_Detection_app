e9bfb41e7e3d1e26f8340c25bdce7d81ae1ceaea
Winduptu <46679904+winduptu@users.noreply.github.com>
2023-03-13 14:43:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -27,47 +27,50 @@ st.set_page_config(page_title='Thai Sentiment Analysis')
 
 
 # Create the main app area
-st.title('Thai Sentiment Analysis')
-text_input = st.text_area('Input text here:', height=200)
-if text_input:
-#     # Preprocess the input text
-#     preprocessed_text = ' '.join([word for word in tokenizer(text_input) if word not in stopwords])
+def rude_app():
+    st.title('Thai Sentiment Analysis')
+    text_input = st.text_area('Input text here:', height=200)
+    if text_input:
+    #     # Preprocess the input text
+    #     preprocessed_text = ' '.join([word for word in tokenizer(text_input) if word not in stopwords])
 
-#     # Convert the preprocessed text into a bag-of-words representation
-#     text_bow = vectorizer.transform([preprocessed_text])
+    #     # Convert the preprocessed text into a bag-of-words representation
+    #     text_bow = vectorizer.transform([preprocessed_text])
 
-#     # Use the trained model to predict sentiment score
-#     predicted_score = model.predict(text_bow)[0]
+    #     # Use the trained model to predict sentiment score
+    #     predicted_score = model.predict(text_bow)[0]
 
-#     weights = pd.DataFrame(model.coef_,  index=model.classes_)
+    #     weights = pd.DataFrame(model.coef_,  index=model.classes_)
 
-#     # Display the predicted sentiment score on the app
-#     st.write('Predicted sentiment score:', predicted_score, weights )
+    #     # Display the predicted sentiment score on the app
+    #     st.write('Predicted sentiment score:', predicted_score, weights )
 
-### new model ###
+    ### new model ###
 
-    feature_vector = featurize(text_input)
-    print(feature_vector)
-    # Convert the feature vector into a feature matrix
-    X = vectorizer.transform(feature_vector)
+        feature_vector = featurize(text_input)
+        print(feature_vector)
+        # Convert the feature vector into a feature matrix
+        X = vectorizer.transform(feature_vector)
 
-    # Get the decision function values for the input text
-    scores = model.decision_function(X)
-    sentiment = model.predict(vectorizer.transform(feature_vector))[0]
+        # Get the decision function values for the input text
+        scores = model.decision_function(X)
+        sentiment = model.predict(vectorizer.transform(feature_vector))[0]
 
-    # Print the score for each word in the input text
-    print(sentiment)
-    for word, score in zip(feature_vector.keys(), scores):
-        print(f'{word}: {score}')
+        # Print the score for each word in the input text
+        print(sentiment)
+        for word, score in zip(feature_vector.keys(), scores):
+            print(f'{word}: {score}')
 
-    if sentiment == 'positive':
-        color = 'green'
-    elif sentiment == 'neutral':
-        color = 'black'
-    else:
-        color = 'red'
+        if sentiment == 'positive':
+            color = 'green'
+        elif sentiment == 'neutral':
+            color = 'black'
+        else:
+            color = 'red'
 
-    st.write('Sentiment: <b style="color:{}">{}</b>'.format(color, sentiment), unsafe_allow_html=True)
-    st.write('Feature vector score: {}'.format(feature_vector), unsafe_allow_html=True)
+        st.write('Sentiment: <b style="color:{}">{}</b>'.format(color, sentiment), unsafe_allow_html=True)
+        st.write('Feature vector score: {}'.format(feature_vector), unsafe_allow_html=True)
 
+
+rude_app()
 
